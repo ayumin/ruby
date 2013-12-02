ayumin/ruby

commit fb3b91048b2ec8aae7402d3a60d9cfcc21b25f04

* hash.c (getenv): use ANSI codepage version of getenv() for miniruby   on Windows.   [ruby-core:58732] [Bug #9189] reported by phasis68 (Heesob Park).
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43950 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

hash.c
@@ -2423,7 +2423,17 @@ static char **my_environ;
 #undef environ
 #define environ my_environ
 #undef getenv
-#define getenv(n) rb_w32_ugetenv(n)
+inline char *
+getenv(const char *name)
+{
+    static int binary = -1;
+    static int locale = -1;
+    if (binary < 0) {
+	binary = rb_ascii8bit_encindex();
+	locale = rb_locale_encindex();
+    }
+    return locale == binary ? rb_w32_getenv(name) : rb_w32_ugetenv(name);
+}
 #elif defined(__APPLE__)
 #undef environ
 #define environ (*_NSGetEnviron())

commit 088335459eece0f28375705eefc7abb92d5a9e0c

* hash.c (getenv): fixed test failures introduced by r43950.   [ruby-core:58774] [Bug #9195] reported by phasis68 (Heesob Park).
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43958 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

hash.c
@@ -2409,7 +2409,7 @@ static char **my_environ;
 #define environ my_environ
 #undef getenv
 inline char *
-getenv(const char *name)
+w32_getenv(const char *name)
 {
     static int binary = -1;
     static int locale = -1;
@@ -2419,6 +2419,7 @@ getenv(const char *name)
     }
     return locale == binary ? rb_w32_getenv(name) : rb_w32_ugetenv(name);
 }
+#define getenv(n) w32_getenv(n)
 #elif defined(__APPLE__)
 #undef environ
 #define environ (*_NSGetEnviron())