ayumin/ruby

commit 4f98ec1a5c243745698a8203ab0027dff6cc15c7

gc.c: memsize of T_NODE
* gc.c (obj_memsize_of): fix size of T_NODE and comments.

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47188 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

gc.c
@@ -2601,18 +2601,24 @@ obj_memsize_of(VALUE obj, int use_tdata)
 	    size += BIGNUM_LEN(obj) * sizeof(BDIGIT);
 	}
 	break;
+
       case T_NODE:
 	switch (nd_type(obj)) {
 	  case NODE_SCOPE:
-	    if (RNODE(obj)->u1.tbl) {
-		/* TODO: xfree(RANY(obj)->as.node.u1.tbl); */
+	    if (RNODE(obj)->nd_tbl) {
+		size += (RNODE(obj)->nd_tbl[0]+1) * sizeof(*RNODE(obj)->nd_tbl);
+	    }
+	    break;
+	  case NODE_ARGS:
+	    if (RNODE(obj)->nd_ainfo) {
+		size += sizeof(*RNODE(obj)->nd_ainfo);
 	    }
 	    break;
 	  case NODE_ALLOCA:
-	    /* TODO: xfree(RANY(obj)->as.node.u1.node); */
-	    ;
+	    size += RNODE(obj)->nd_cnt * sizeof(VALUE);
+	    break;
 	}
-	break;			/* no need to free iv_tbl */
+	break;
 
       case T_STRUCT:
 	if ((RBASIC(obj)->flags & RSTRUCT_EMBED_LEN_MASK) == 0 &&

commit 6344ce7eeea19dc76386f41104dcbdebc878a62f

* gc.c (obj_memsize_of): don't calculate memsize of T_NODE   when called from check_gen_consistency. It fixes segmentation   fault on RGENGC_CHECK_MODE >= 1 introduced by r47188.
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47208 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

gc.c
@@ -690,7 +690,7 @@ static void gc_mark(rb_objspace_t *objspace, VALUE ptr);
 static void gc_mark_maybe(rb_objspace_t *objspace, VALUE ptr);
 static void gc_mark_children(rb_objspace_t *objspace, VALUE ptr);
 
-static size_t obj_memsize_of(VALUE obj, int use_tdata);
+static size_t obj_memsize_of(VALUE obj, int use_all_types);
 static VALUE gc_verify_internal_consistency(VALUE self);
 
 static double getrusage_time(void);
@@ -2497,7 +2497,7 @@ size_t rb_generic_ivar_memsize(VALUE);
 #include "regint.h"
 
 static size_t
-obj_memsize_of(VALUE obj, int use_tdata)
+obj_memsize_of(VALUE obj, int use_all_types)
 {
     size_t size = 0;
 
@@ -2557,7 +2557,7 @@ obj_memsize_of(VALUE obj, int use_tdata)
 	}
 	break;
       case T_DATA:
-	if (use_tdata) size += rb_objspace_data_type_memsize(obj);
+	if (use_all_types) size += rb_objspace_data_type_memsize(obj);
 	break;
       case T_MATCH:
 	if (RMATCH(obj)->rmatch) {
@@ -2590,7 +2590,7 @@ obj_memsize_of(VALUE obj, int use_tdata)
 	break;
 
       case T_NODE:
-	size += rb_node_memsize(obj);
+	if (use_all_types) size += rb_node_memsize(obj);
 	break;
 
       case T_STRUCT: